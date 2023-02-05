3cb2ce8e9bc0b5023a9ca2aa3ef525ab43f7551e
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-02-05 03:11:08 -0800

    done

--- chatboxV2/app.py
+++ chatboxV2/app.py
@@ -1,78 +1,131 @@
 from tkinter import *
+from PIL import ImageTk, Image
 from chat import get_response, bot_name
+#import tkfont as font
+#root = tk()
 
+#Colors
+BG_BLACK = "#282828"
+BG_BLUEBLACK = "#323232"
 BG_GRAY = "#ABB2B9"
-BG_COLOR = "#17202A"
-TEXT_COLOUR = "#EAECEE"
+BG_YELLOW = "#F4B87F"
+BG_BLUE = "#17202A"
+BG_PINK = "#D2A5A1"
+BG_LIGHTYELLOW = "#FAF8F7"
+BG_LIGHTBLUE = "#A9CCD7"
+BG_WHITE = "#FEFFFF"
+BG_LIGHTORANGE = "#FAECD8"
+
+#Images#
+# serene_image = PhotoImage(file = '//Users//manikanta///Downloads//serenelogo.png')
+
+
+#Text
+TEXT_COLOUR = "#000000"
+TEXT_REDCOLOR = "#EE2727"
+
 
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
-
 
 class ChatApplication:
     def __init__(self):
         self.window = Tk()
         self._setup_main_window()
-
+    
     def run(self):
         self.window.mainloop()
 
+    #upper text widget
     def _setup_main_window(self):
         self.window.title("Chat")
         self.window.resizable(width=False, height=False)
-        self.window.configure(width=600, height=750, bg=BG_COLOR)
+        self.window.configure(width=600, height=750, bg=BG_BLUEBLACK)
 
-        # head label
-        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOUR,
-                           text="Welcome", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
+        # head label logo image------------------
+        image1 = Image.open("./serenelogo.png")
 
-        # tiny divider
-        line = Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        newsize = (200, 200)
+        image1 = image1.resize(newsize)
 
-        # text widget
-        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOUR,
+        test = ImageTk.PhotoImage(image1)
+
+        label1 = Label(image=test)
+        label1.image = test
+
+            # Position image
+        label1.place(x= 200, y = -70)
+
+        #send button image-----------------------
+        image1 = Image.open("./sendPng.png")
+
+        newsize = (30, 30)
+        image1 = image1.resize(newsize)
+
+        test2 = ImageTk.PhotoImage(image1)
+
+        label1 = Label(image=test2)
+        label1.image = test2
+
+        # Position image
+        #label1.place(x=200, y=-70)
+
+
+
+        # frame = Frame(self.window, width=600, height=400)
+        # frame.pack()
+        # frame.place(anchor='center', relx=0.5, rely=0.5)
+        #
+        # img = ImageTk.PhotoImage(Image.open('./serenelogo.png'))
+        # head_label = Label(frame, image=img)
+        # head_label.pack()
+        # head_label = Label(self.window, bg=BG_BLUE, fg=TEXT_COLOUR,
+        #                     text="SERENE", font=FONT_BOLD, pady=10)
+        # head_label.place(relwidth=1)
+
+        #text widget Middle
+        self.text_widget = Text(self.window, width=20, height=2, bg=BG_LIGHTORANGE, fg=TEXT_COLOUR,
                                 font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.text_widget.place(relheight=0.825, relwidth=1, rely=0.08)
         self.text_widget.configure(cursor="arrow", state=DISABLED)
 
-        # bottom label
-        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
-        bottom_label.place(relwidth=1, rely=0.825)
+        #bottom label
+        bottom_label = Label(self.window, bg=BG_BLUE, height=85)
+        bottom_label.place(relwidth=1, rely=0.890)
 
-        # message entry box
-        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOUR, font=FONT)
-        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        #message entry box
+        self.msg_entry = Entry(bottom_label, bg=BG_LIGHTBLUE, fg=TEXT_COLOUR, font=FONT)
+        self.msg_entry.place(relwidth=0.78, relheight=0.03, rely=0.0098, relx=0.011)
         self.msg_entry.focus()
         self.msg_entry.bind("<Return>", self._on_enter_pressed)
 
-        # send button
-        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY,
-                             command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
-
+        #send button
+        send_button = Button(bottom_label, image=test2, font=FONT_BOLD, width=20, bg=BG_WHITE,
+                             command= lambda: self._on_enter_pressed(None))
+        send_button.place(relx=0.83, rely=0.0098, relheight=0.03, relwidth=0.12)
+        
     def _on_enter_pressed(self, event):
         msg = self.msg_entry.get()
-        self._insert_message(msg, "You")
+        self._insert_message(msg, "USER")
 
     def _insert_message(self, msg, sender):
         if not msg:
             return
 
         self.msg_entry.delete(0, END)
-        msg1 = f"{sender}: {msg}\n\n"
+
+        msg1 = f"{sender} >> {msg}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg1)
         self.text_widget.configure(state=DISABLED)
 
-        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+        msg2 = f"{bot_name} >> {get_response(msg)}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg2)
+
         self.text_widget.configure(state=DISABLED)
 
         self.text_widget.see(END)
-
 
 if __name__ == "__main__":
     app = ChatApplication()
---
+++ chatboxV2/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+#python3 train.py
+#python3 chat.py
+python3 app.py
