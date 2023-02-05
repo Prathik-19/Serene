c7dc4927e53a57cd9232df0357b5ccb43d365ef3
Manikanta Illuri <manikanta.illuri2013@gmail.com>
2023-02-05 03:33:55 -0800

    fnl

--- chatboxV2/app.py
+++ chatboxV2/app.py
@@ -114,12 +114,12 @@ class ChatApplication:
 
         self.msg_entry.delete(0, END)
 
-        msg1 = f"{sender} >> {msg}\n\n"
+        msg1 = f"{sender} : {msg}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg1)
         self.text_widget.configure(state=DISABLED)
 
-        msg2 = f"{bot_name} >> {get_response(msg)}\n\n"
+        msg2 = f"{bot_name} : {get_response(msg)}\n\n"
         self.text_widget.configure(state=NORMAL)
         self.text_widget.insert(END, msg2)
 
